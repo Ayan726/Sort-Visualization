b66b97af959818801d5e24c842598d77cf1257cd
Ayan726 <95524790+Ayan726@users.noreply.github.com>
2023-03-31 17:44:29 +0530

    Add files via upload

---
+++ Tailwind CSS/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero.jpg')"
+      }
+    }
+  },
+  plugins: [],
+}
